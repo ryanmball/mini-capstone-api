11a23e2f6943cfb235359e85f2956c28c373809f
ryan <ball.ryan.rb@gmail.com>
2021-05-10 13:04:11 -0600

    adds create route and method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,17 @@ class ProductsController < ApplicationController
   def index
     render json: Product.all.sort_by { |product| product[:id] }.as_json
   end
+
+  def create
+    product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    product.save
+    render json: product.as_json
+  end
   
   def show
     render json: Product.find(params[:id]).as_json
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get "products" => "products#index"
   # diplays all products sorted by index number
   
+  post "products" => "products#create"
+  # post request to create a new product
+
   get "products/:id" => "products#show"
   # displays a single product based on an id number passed through a url segment param
 end
