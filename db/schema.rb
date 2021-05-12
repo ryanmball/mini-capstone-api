1fbada4c165e35b545668f32ca73988c56e6cd23
ryan <ball.ryan.rb@gmail.com>
2021-05-12 12:41:23 -0600

    adds in_stock and inventory columns to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_181816) do
+ActiveRecord::Schema.define(version: 2021_05_12_183848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2021_05_12_181816) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "in_stock", default: true
+    t.integer "inventory"
   end
 
 end
