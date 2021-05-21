7a4b3839347ccf1c97bf4d5e8194815cfee716a3
ryan <ball.ryan.rb@gmail.com>
2021-05-21 14:12:13 -0600

    adds 'belongs_to :product' in order serializer

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,3 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :product_id, :quantity, :subtotal, :tax, :total, :product
+  attributes :id, :product_id, :quantity, :subtotal, :tax, :total
+
+  belongs_to :product
 end
