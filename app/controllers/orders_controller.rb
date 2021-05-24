5e94b2f327e0e46bd5bf1e42992c7f60a33bd3b5
ryan <ball.ryan.rb@gmail.com>
2021-05-24 10:24:45 -0600

    adds before_action to authenticate user for orders methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,28 +1,23 @@
 class OrdersController < ApplicationController
+
+  before_action :authenticate_user
+
   def index
-    if current_user
-      render json: current_user.orders
-    else
-      render json: {}, status: :unauthorized
-    end
+    render json: current_user.orders
   end
 
   def create
-    if current_user
-      order = Order.new(
-        user_id: current_user.id,
-        product_id: params[:product_id],
-        quantity: params[:quantity]
-      )
-      order.save
-      order.subtotal = order.calc_subtotal
-      order.tax = order.calc_tax
-      order.total = order.calc_total
-      order.save
-      render json: order
-    else
-      render json: {}, status: :unauthorized
-    end
+    order = Order.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity]
+    )
+    order.save
+    order.subtotal = order.calc_subtotal
+    order.tax = order.calc_tax
+    order.total = order.calc_total
+    order.save
+    render json: order
   end
 
   def show
